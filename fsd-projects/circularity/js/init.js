3529734d77a131ce26b011d386fdded4733b6467
Sebastian0426 <fahrig913@wearescpps.org>
2025-11-05 17:30:49 +0000

    completed circularity challenge

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -84,17 +84,17 @@ for(var i = 0; i < 1000; i++){
         game.checkCirclePosition = function(circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
-                circle.x = 0;
+            if ( circle.x - circle.radius > canvas.width ) {
+                circle.x = 0 - circle.radius;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if (circle.x < 0){
-                circle.x = canvas.width
-            } else if (circle.y < 0){
-                circle.y = canvas.height
-            } else if(circle.y > canvas.height){
-                circle.y = 0
+            if (circle.x + circle.radius < 0){
+                circle.x = canvas.width + circle.radius
+            } else if (circle.y + circle.radius < 0){
+                circle.y = canvas.height + circle.radius
+            } else if(circle.y - circle.radius > canvas.height){
+                circle.y = 0 - circle.radius
             }
 
 
